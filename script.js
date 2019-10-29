f25b27f20266d015430a032a6be881e11a80fd48
IharSa <iharsaulich@gmail.com>
2019-10-29 13:20:07 +0300

    start

---
+++ script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction () {
+    let x = document.getElementById ('myTopNav');
+
+    if (x.className === "top-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "top-nav";
+    }
+}
